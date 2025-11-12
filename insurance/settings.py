d6458e1c9238b48cc118a51ed5b8dc18fbc87b9e
DEVARUN98 <arundev8086@gmail.com>
2025-11-12 15:53:10 +0530

    added frontend url for netlify new

--- insurance/settings.py
+++ insurance/settings.py
@@ -55,7 +55,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
     "http://127.0.0.1:3002",
     "https://ib-frontend-er7a.onrender.com",
-    "https://insurebasket.netlify.app/"
+    "https://insurebasket.netlify.app",
 ]
 
 
